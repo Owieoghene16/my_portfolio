da1c7671905480fa9d1086525e6e7178534f82fa
Okpugie Kindness <64986593+Owieoghene16@users.noreply.github.com>
2024-05-03 11:13:28 +0100

    Update Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -45,7 +45,7 @@ const Home = () => {
             </h1>
               <div className="links">
                 <a
-                  href="https://www.linkedin.com/"
+                  href="https://www.linkedin.com/in/kindness-okpugie-754b20232/"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
